de5c0922c9d6f3e25c99582e4f2254c7f9635821
Rohit Akurdekar <114896370+RohitAkurdekar@users.noreply.github.com>
2025-11-03 20:15:29 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,15 @@
-self.addEventListener("install", event => {
+const CACHE_NAME = "roha-expense-cache-v1";
+const urlsToCache = ["./", "./index.html", "./style.css", "./app.js"];
+
+self.addEventListener("install", (event) => {
   console.log("💾 Service Worker installed");
   event.waitUntil(
-    caches.open("expense-cache").then(cache => {
-      return cache.addAll([
-        "./",
-        "index.html",
-        "app.js",
-        "style.css",
-        "manifest.json"
-      ]);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then((resp) => resp || fetch(event.request))
   );
 });
